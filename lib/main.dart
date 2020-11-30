180b932b85d43975f0cb9bef0bb4a7e3c2e7f7ca
Yurv25 <yurialejandro.valverde@gmail.com>
2020-11-30 15:10:20 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    FriendlyChatApp(),
+  );
+}
+
+String _name = 'Yuri';
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'FriendlyChat', home: ChatScreen());
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text}); // NEW
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  final List<ChatMessage> _messages = [];
+  final FocusNode _focusNode = FocusNode(); // NEW
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('FriendlyChat')),
+      body: Column(
+        // MODIFIED
+        children: [
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0), // NEW
+              reverse: true, // NEW
+              itemBuilder: (_, int index) => _messages[index], // NEW
+              itemCount: _messages.length, // NEW
+            ), // NEW
+          ), // NEW
+          Divider(height: 1.0), // NEW
+          Container(
+            // NEW
+            decoration:
+                BoxDecoration(color: Theme.of(context).cardColor), // NEW
+            child: _buildTextComposer(), //MODIFIED
+          ), // NEW
+        ], // NEW
+      ), // NEW
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+              focusNode: _focusNode,
+            ),
+          ),
+          IconTheme(
+            data: IconThemeData(color: Theme.of(context).accentColor),
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      //NEW
+      text: text, //NEW
+    ); //NEW
+    setState(() {
+      //NEW
+      _messages.insert(0, message); //NEW
+    });
+    _focusNode.requestFocus();
+  }
+}
