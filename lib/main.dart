619b93f3195f84dce7ce5b891d870f9094ab5bb4
Yurv25 <yurialejandro.valverde@gmail.com>
2020-12-22 13:23:39 +0100

    adding animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(
     FriendlyChatApp(),
   );
 }
+
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 String _name = 'Yuri';
 
@@ -15,36 +28,49 @@ class FriendlyChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'FriendlyChat', home: ChatScreen());
+    return MaterialApp(
+        title: 'FriendlyChat',
+        theme: defaultTargetPlatform == TargetPlatform.iOS // NEW
+            ? kIOSTheme // NEW
+            : kDefaultTheme,
+        home: ChatScreen());
   }
 }
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text}); // NEW
+  ChatMessage({this.text, this.animationController}); // NEW
   final String text;
+  final AnimationController animationController;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(child: Text(_name[0])),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(_name, style: Theme.of(context).textTheme.headline4),
-              Container(
-                margin: EdgeInsets.only(top: 5.0),
-                child: Text(text),
+    return SizeTransition(
+      sizeFactor: CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut), // NEW
+      axisAlignment: 0.0,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(_name, style: Theme.of(context).textTheme.headline4),
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -55,36 +81,48 @@ class ChatScreen extends StatefulWidget {
   _ChatScreenState createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final _textController = TextEditingController();
 
   final List<ChatMessage> _messages = [];
-  final FocusNode _focusNode = FocusNode(); // NEW
+  final FocusNode _focusNode = FocusNode();
+  bool _isComposing = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('FriendlyChat')),
-      body: Column(
-        // MODIFIED
-        children: [
-          Flexible(
-            child: ListView.builder(
-              padding: EdgeInsets.all(8.0), // NEW
-              reverse: true, // NEW
-              itemBuilder: (_, int index) => _messages[index], // NEW
-              itemCount: _messages.length, // NEW
-            ), // NEW
-          ), // NEW
-          Divider(height: 1.0), // NEW
-          Container(
-            // NEW
-            decoration:
-                BoxDecoration(color: Theme.of(context).cardColor), // NEW
-            child: _buildTextComposer(), //MODIFIED
-          ), // NEW
-        ], // NEW
-      ), // NEW
+      appBar: AppBar(
+        title: Text('FriendlyChat'),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Container(
+          child: Column(
+            // MODIFIED
+            children: [
+              Flexible(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8.0), // NEW
+                  reverse: true, // NEW
+                  itemBuilder: (_, int index) => _messages[index], // NEW
+                  itemCount: _messages.length, // NEW
+                ), // NEW
+              ), // NEW
+              Divider(height: 1.0), // NEW
+              Container(
+                // NEW
+                decoration:
+                    BoxDecoration(color: Theme.of(context).cardColor), // NEW
+                child: _buildTextComposer(), //MODIFIED
+              ), // NEW
+            ], // NEW
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS // NEW
+              ? BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Colors.grey[200]), // NEW
+                  ),
+                )
+              : null),
     );
   }
 
@@ -96,7 +134,14 @@ class _ChatScreenState extends State<ChatScreen> {
           Flexible(
             child: TextField(
               controller: _textController,
-              onSubmitted: _handleSubmitted,
+              onChanged: (String text) {
+                // NEW
+                setState(() {
+                  // NEW
+                  _isComposing = text.length > 0; // NEW
+                }); // NEW
+              },
+              onSubmitted: _isComposing ? _handleSubmitted : null,
               decoration: InputDecoration.collapsed(hintText: 'Send a message'),
               focusNode: _focusNode,
             ),
@@ -104,12 +149,23 @@ class _ChatScreenState extends State<ChatScreen> {
           IconTheme(
             data: IconThemeData(color: Theme.of(context).accentColor),
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 4.0),
-              child: IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text)),
-            ),
-          )
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoButton(
+                        // NEW
+                        child: Text('Send'), // NEW
+                        onPressed: _isComposing // NEW
+                            ? () =>
+                                _handleSubmitted(_textController.text) // NEW
+                            : null,
+                      )
+                    : IconButton(
+                        icon: const Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )),
+          ),
         ],
       ),
     );
@@ -117,14 +173,30 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false; // NEW
+    });
     ChatMessage message = ChatMessage(
       //NEW
-      text: text, //NEW
+      text: text,
+      animationController: AnimationController(
+        // NEW
+        duration: const Duration(milliseconds: 700), // NEW
+        vsync: this, // NEW
+      ), //NEW
     ); //NEW
     setState(() {
       //NEW
       _messages.insert(0, message); //NEW
     });
     _focusNode.requestFocus();
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
   }
 }
